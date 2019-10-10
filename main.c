75946b80c6d8d287ddb9133086997aebd2b20440
suhaa99 <jjangsoo99@naver.com>
2019-10-10 16:00:52 +0900

    02&03

--- main.c
+++ main.c
@@ -3,19 +3,43 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void print_star()
+int sumTwo(int a, int b)
 {
-	int i;
-	
-	for(i=0; i<10; i++)
-	 printf("*");
+	return (a+b);
 }
+
+//===================================================
+
+int square(int n)
+{
+	return (n*n);
+}
+
+//====================================================
+
+int get_max(int a, int b)
+{
+	if(a>b) return a;
+	else return b;
+}
+
+//====================================================
 
 int main(int argc, char *argv[]) {
 	
-	print_star();
-	print_star();
-	print_star();
+	int a, b;
+	int output;
 	
+	a=2;
+	b=5;
+	
+	output = sumTwo(a,b);
+	printf("sumTwo : %i\n", output);
+	
+	printf("square : %i\n", square(a));    
+	
+	printf("get_max : %i\n", get_max(a,b));
+	
+
 	return 0;
 }
